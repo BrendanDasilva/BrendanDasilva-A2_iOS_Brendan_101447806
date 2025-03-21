0a22c30669b8092467875794f330a7691a3a06a5
Brendan Dasilva <brendan.dasilva@gmail.com>
2025-03-21 11:46:00 -0400

    Initialize Core Data stack with PersistenceController

--- A2_iOS_Brendan_101447806/Persistence.swift
+++ A2_iOS_Brendan_101447806/Persistence.swift
@@ -6,3 +6,31 @@
 //
 
 import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    // In-memory store for previews
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        return result
+    }()
+    
+    // Core Data container
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "A2_iOS_Brendan_101447806")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
